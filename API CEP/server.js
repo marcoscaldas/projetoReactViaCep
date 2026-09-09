200d4664a30896b7e361b17cb8b3ed31111d5bea
aluno <e023aluno@outlook.com>
2026-09-09 17:14:57 -0300

    Parte 02 Backend Finalizado

--- API CEP/server.js
+++ API CEP/server.js
@@ -1,1 +1,31 @@
+import express from "express";
+import cors from "cors";
+import fs from "fs";
 
+const port = 3001;
+
+const app = express();
+app.use(cors());
+
+const ceps = JSON.parse(
+  fs.readFileSync("./data/ceps.json", "utf-8")
+);
+
+app.get("/cep/:cep", (req, res) => {
+  const cepBuscado = req.params.cep.replace(/\D/g, "");
+
+  const endereco = ceps.find(c => c.cep === cepBuscado);
+
+  if (!endereco) {
+    return res.status(404).json({
+      erro: true,
+      mensagem: "CEP não encontrado"
+    });
+  }
+
+  res.json(endereco);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
